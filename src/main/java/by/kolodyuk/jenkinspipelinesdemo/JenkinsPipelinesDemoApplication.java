2da73bb4ecefd3ca3e4507bd08d1a778bef16bd3
Stas Kolodyuk <staskolodyuk@gmail.com>
2018-09-28 02:36:50 +0300

    initial pipeline

---
+++ src/main/java/by/kolodyuk/jenkinspipelinesdemo/JenkinsPipelinesDemoApplication.java
@@ -0,0 +1,12 @@
+package by.kolodyuk.jenkinspipelinesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelinesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelinesDemoApplication.class, args);
+	}
+}
